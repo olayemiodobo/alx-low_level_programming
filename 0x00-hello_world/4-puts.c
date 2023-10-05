55b9a9658061a70f052cd963dbe76e716c739d18
olayemi odobo <odoboyemi@gmail.com>
2023-10-05 08:13:39 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always O
+ */
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle);
