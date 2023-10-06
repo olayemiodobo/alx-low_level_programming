71ceab0d1982c605d3aefc9c9eb9d77c081aa8c3
olayemi odobo <odoboyemi@gmail.com>
2023-10-05 17:51:58 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is what the main does
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
