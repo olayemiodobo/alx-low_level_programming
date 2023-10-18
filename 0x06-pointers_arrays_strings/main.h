74f1ec372ede7b34bd002945e066e81d71e1103a
olayemi odobo <odoboyemi@gmail.com>
2023-10-18 09:29:35 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ char *string_toupper(char *a);
 char *cap_string(char *a);
 char *leet(char *a);
 char *rot13(char *a);
-voud print_number(int n);
+void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
