228fcc60dc30c8ff3c74b6f23c4528666c50e659
olayemi odobo <odoboyemi@gmail.com>
2023-11-02 06:05:20 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,12 +10,13 @@
  */
 int check_num(char *str)
 {
+	/*Declaring variables*/
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str))
+	while (count < strlen(str))/*count string*/
 	{
-		if (!isdigit(str[count]))
+		if (!isdigit(str[count]))/*check if str there are digit*/
 		{
 			return (0);
 		}
@@ -39,14 +40,14 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	count = 1;
-	while (count < argc)
+	while (count < argc)/*Goes through the whole array*/
 	{
 	if (check_num(argv[count]))
 	{
-	str_to_int = atoi(argv[count]);
-	}
+	str_to_int = atoi(argv[count]);/*ATOI --> convert string to int*/
 	sum += str_to_int;
 	}
+	/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 		printf("Error\n");
@@ -54,6 +55,6 @@ int main(int argc, char *argv[])
 		}
 		count++;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", sum);/*print sum*/
 	return (0);
 }
